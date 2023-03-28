6d608fc414462bdcbcbbadeeabd9ab8655dd05e3
vikyw <vikyw89@gmail.com>
2023-03-28 16:07:48 +0800

    fix: setConfig

--- package/config.js
+++ package/config.js
@@ -2,13 +2,22 @@ import { initializeApp } from "firebase/app";
 
 // TODO: Replace the following with your app's Firebase project configuration
 
-export let firebaseConfig = {
+let firebaseConfig = {
   apiKey: null,
   authDomain: null,
   projectId: null,
   storageBucket: null,
   messagingSenderId: null,
   appId: null,
+};
+
+/**
+ * Sets the Firebase configuration object.
+ * @param {Object} config - The Firebase configuration object.
+ * @returns {void}
+ */
+export const setFirebaseConfig = (config) => {
+  firebaseConfig = config;
 };
 
 export const app = initializeApp(firebaseConfig);
